7dabc555a0868047bd9bc90031564ebcaad29208
nurzhakhan <190103374@stu.sdu.edu.kz>
2021-03-03 09:20:13 +0600

    fourth commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Student;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,14 @@ Route::get('/', function () {
 
 Route::get('/insert', function() {
     DB::insert("insert into student(name, date_of_birth, gpa, advisor) values('Nurzhakhan', '2002-03-28', 3.5, 'Zhangir Raev')");
+});
+
+Route::get('/insert2', function() {
+    $student = new Student;
+
+    $student->name='Arman';
+    $student->date_of_birth='2002-02-24';
+    $student->gpa=3.8;
+    $student->advisor='Maksat';
+    $student->save();
 });
